1e7012b55559374e8877d08c7274179f4ba0604e
RonyFonseca <ronyfonsecadelima24@gmail.com>
2026-09-19 21:51:26 -0300

    feat: Muda a cor do menu para uma primaria

--- mobile/components/Drawer/CreateDrawer.tsx
+++ mobile/components/Drawer/CreateDrawer.tsx
@@ -1,5 +1,5 @@
 import {View, Text, Pressable} from "react-native";
-import { NotePencilIcon,LightbulbIcon } from "phosphor-react-native";
+import { NotePencilIcon,LightbulbIcon, X, XIcon } from "phosphor-react-native";
 import {colors} from "../../theme/Colors";
 import {StyleSheet} from "react-native";
 import {useState} from "react";
@@ -13,27 +13,42 @@ export default function CreateDrawer({ visible,close }: CreateDrawerProps) {
 
   
     return (
-        <View className={visible ? 'flex bg-white w-full rounded-t-3xl p-4 h-35' : 'hidden'}>
+        <View className={visible ? 'flex bg-white w-full rounded-t-3xl p-4 h-35 border-24' : 'hidden'}>
             <View className="flex-row gap-6 items-center mb-4">
                 <Pressable onPress={close}>
-                    <Text>x</Text>
+                    <View style={styles.containerX}>
+                        <XIcon/>
+                    </View>
                 </Pressable>
-                <Text>O que você deseja criar?</Text>
+                <View>
+                    <Text className="text-large-text color-text-gray-800">
+                        O que você deseja criar?
+                    </Text>
+                </View>
             </View>
         
             <View style={styles.conteinerButtons}>
-                <Pressable >
-                    <View style={styles.containerIcon}>
-                        <NotePencilIcon/>
-                    </View>
-                    <Text>Post</Text>
-                </Pressable>
-                <Pressable>
+                
+                <View className="items-center">
+                    <Pressable className="items-center">
+                        <View style={styles.containerIcon}>
+                            <NotePencilIcon/>
+                        </View>
+                        <Text className="text-body-text color-text-gray-800">
+                            Post
+                        </Text>
+                    </Pressable>
+                </View>
+
+                <Pressable className="items-center">
                     <View style={styles.containerIcon}>
                         <LightbulbIcon/>
                     </View>
-                    <Text>Evento</Text>
+                    <Text className="text-body-text color-text-gray-800">
+                        Idea
+                    </Text>
                 </Pressable>
+            
             </View>
         </View>
     )
@@ -42,11 +57,18 @@ export default function CreateDrawer({ visible,close }: CreateDrawerProps) {
 const styles = StyleSheet.create({
     containerIcon: {
         backgroundColor: colors.primary[50],
-        width: 50,
-        height: 50,
-        borderRadius: 5,
+        width: 70,
+        height: 70,
+        borderRadius: 24,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    containerX: {
+        width: 30,
+        height: 30,
+        //borderRadius: 24,
+        //justifyContent: 'center',
+        //alignItems: 'center',
     },
     conteinerButtons: {
         flexDirection: 'row',
